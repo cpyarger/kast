481accbac2821bb9003cc9876c8988c0451f77d8
chris yarger <cpyarger@gmail.com>
2015-01-14 20:18:07 -0500

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "iostream"
 
+
+//#include <QGst/Init>
+//#include <QGst/Bus>
+//#include <QGst/Pipeline>
+//#include <QGst/Parse>
+//#include <QGst/Message>
+//#include <QGst/Utils/ApplicationSink>
+//#include <QGst/Utils/ApplicationSource>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +20,25 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_StreamQuality_valueChanged(int value)
+{
+std::cout << " Vol:" << value << "\n" ;
+}
+
+//void MainWindow::on_actionAbout_triggered()
+//{
+//ui(new Ui::about.ui);
+//}
+
+
+void MainWindow::on_buttonCut_pressed()
+{
+ std::cout << " Cut button Pressed"  << "\n" ;
+}
+
+void MainWindow::on_buttonFade_pressed()
+{
+     std::cout << " Fade button Pressed"  << "\n" ;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_StreamQuality_valueChanged(int value);
+    void on_actionAbout_triggered();
+
+    void on_buttonCut_clicked();
+
+    void on_buttonCut_pressed();
+
+    void on_buttonFade_pressed();
+
 private:
     Ui::MainWindow *ui;
 };
