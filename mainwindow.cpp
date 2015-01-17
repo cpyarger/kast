76e4c1327212e26528909d9785087cf7713a2ea4
chris yarger <cpyarger@gmail.com>
2015-01-17 00:28:47 -0500

    Updates on gstreamer libraries

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,30 @@
 //#include <QGst/Message>
 //#include <QGst/Utils/ApplicationSink>
 //#include <QGst/Utils/ApplicationSource>
+
+#include <iostream>
+#include <QCoreApplication>
+//#include <QGlib/Error>
+//#include <QGlib/Connect>
+//#include <QGst/Init>
+//#include <QGst/Bus>
+//#include <QGst/Pipeline>
+//#include <QGst/Parse>
+//#include <QGst/Message>
+//#include <QGst/Utils/ApplicationSink>
+//#include <QGst/Utils/ApplicationSource>
+
+#include <QtGStreamer/QGlib/Error>
+#include <QtGStreamer/QGlib/Connect>
+#include <QtGStreamer/QGst/Init>
+#include <QtGStreamer/QGst/Bus>
+#include <QtGStreamer/QGst/Pipeline>
+#include <QtGStreamer/QGst/Parse>
+#include <QtGStreamer/QGst/Message>
+#include <QtGStreamer/QGst/Utils/ApplicationSink>
+#include <QtGStreamer/QGst/Utils/ApplicationSource>
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
